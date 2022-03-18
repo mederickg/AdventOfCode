e0a4fb2b7787e755cc661a0eca244994253374bc
Médérick Charles Fernand Grivel <100237137+mederickg@users.noreply.github.com>
2022-03-18 00:22:05 -0400

    Update day1.py

--- AOC/day1.py
+++ AOC/day1.py
@@ -24,10 +24,4 @@ def sliding_sweep(file):
                 prev_chunk = value
                 print(f"count: {count}")
                 del(list[0])   
-<<<<<<< HEAD
-=======
 
->>>>>>> db6a7e8c68c27f30be130f703f606d9c857adfb8
-            
-            
-sliding_sweep("/Users/mederickschool/Desktop/CodingProjects/AOC/day1.txt")
